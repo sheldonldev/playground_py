9c3fb3391bb4e5c6baff7da3c2f8e9c90fc8891b
sheldonldev <xiadanli0320@gmail.com>
2024-02-16 10:48:22 +0800

    add machine learning

--- src/machine_learning/hoeffding.py
+++ src/machine_learning/hoeffding.py
@@ -1,8 +1,8 @@
 import math
 
 
-def get_finit_hoeffeding_bound(epsilon, data_num, hypothesis_num=1):
-    return 2 * hypothesis_num * math.exp(-2 * data_num * math.pow(epsilon, 2))
+def get_finit_hoeffeding_bound(error_bound, data_num, hypothesis_num=1):
+    return 2 * hypothesis_num * math.exp(-2 * data_num * math.pow(error_bound, 2))
 
 
 if __name__ == '__main__':
